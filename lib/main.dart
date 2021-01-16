f7c19df13bafa0dea1d297aa0d268502d8f46760
droidmedi <mehdimedi555@gmail.com>
2021-01-16 19:26:36 +0100

    walo ghir lbdya

--- lib/main.dart
+++ lib/main.dart
@@ -98,7 +98,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter SelectFormField Demo'),
+        title: Text('svoice counter'),
       ),
       body: SingleChildScrollView(
         padding: EdgeInsets.only(left: 20, right: 20, top: 10),
